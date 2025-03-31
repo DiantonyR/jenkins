b97ab55ed59816ec00c9f1d0d988b542025d45e0
DiantonyR <diantonylavado@gmail.com>
2025-03-31 12:39:40 -0500

    Primer commit

--- mi-backent-copia-copia/config/db.js
+++ mi-backent-copia-copia/config/db.js
@@ -8,7 +8,7 @@ class connection {
     async connect() {
         if (!this.pool) {
             this.pool = mysql2.createPool({
-                host: process.env.DB_HOST || 'mysql',
+                host: process.env.DB_HOST ,
                 user: process.env.DB_USER,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
